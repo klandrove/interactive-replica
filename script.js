d8a604a3131c0d7aaddd2d9584d4aa0791acb0d3
KaitLand12 <kmlandrove@gmail.com>
2024-10-17 11:49:04 -0400

    Add sticky navbar

--- script.js
+++ script.js
@@ -126,6 +126,19 @@ var cursor = {
 
 cursor.init();
 
+// From w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+// keep track of previous scroll position
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 // ---------------------------------------------------------------------------
 const radioButtons = document.querySelectorAll('input[name="sort"]');
